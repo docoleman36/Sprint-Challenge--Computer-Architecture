eb11c7a0c74eafc47c47fd7bca1faa7f9ce20cff
DJ Coleman <DJ@DJs-MacBook-Pro.local>
2020-12-13 20:25:15 -0800

    finished sprint

---
+++ ls8/ls8.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+"""Main."""
+
+import sys
+from cpu import *
+
+cpu = CPU()
+
+# hard add argument for debugging purposes
+# no command line args in vs2019 :(
+#sys.argv.append('C:\\Users\\super pawn\\Desktop\\python projects\\Computer-Architecture\\ls8\\examples\print8.ls8')
+#sys.argv.append('C:\\Users\\super pawn\\Desktop\\python projects\\Computer-Architecture\\ls8\\examples\mult.ls8')
+#sys.argv.append('C:\\Users\\super pawn\\Desktop\\python projects\\Computer-Architecture\\ls8\\examples\call.ls8')
+sys.argv.append('C:\\Users\\super pawn\\Desktop\\python projects\\Sprint-Challenge--Computer-Architecture\sctest.ls8')
+
+if len(sys.argv) > 1:
+    cpu.load(sys.argv[1])
+    cpu.run()
+else:
+    print("please add a program to run")
